46c94f6e0148f2b96a729b8c0f0b1ced18e54444
Nonton Anime <artdengun@gmail.com>
2021-02-20 09:04:24 +0700

    konsep spring web service, dimana kita bisa costume sesuai kebutuhan

---
+++ src/main/java/com/deni/springRESTfull/web/service/SpringResTfullWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.deni.springRESTfull.web.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringResTfullWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringResTfullWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deni/springRESTfull/web/service/SpringResTfullWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deni.springRESTfull.web.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringResTfullWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
